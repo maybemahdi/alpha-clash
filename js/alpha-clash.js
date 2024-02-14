d0922eac0640e62baa98036678550676c9144211
maybemahdi <mh7266391@gmail.com>
2024-02-15 02:09:27 +0600

    Completed

--- js/alpha-clash.js
+++ js/alpha-clash.js
@@ -1,8 +1,62 @@
 document.getElementById("playBtn").addEventListener("click", function () {
   hideClass("home");
   showClass("playground");
+  continueGame();
+});
+
+// continue game
+function continueGame() {
   let alphabet = getRandomAlphabet();
   let alphabetField = document.getElementById("alphabetShow");
   alphabetField.innerText = alphabet;
   setBg(alphabet);
+}
+document.addEventListener("keyup", function (event) {
+  let pressedKey = event.key;
+  let expectedKey = document
+    .getElementById("alphabetShow")
+    .innerText.toLowerCase();
+
+  // condition for stop the game
+  if (pressedKey === "Escape") {
+    gameOver();
+    document
+      .getElementById("play-again")
+      .addEventListener("click", function () {
+        playAgain();
+      });
+  }
+  //main condition
+  if (pressedKey === expectedKey) {
+    removeBg(expectedKey);
+    //get current score
+    let currentScoreElement = document.getElementById("score");
+    let currentScoreText = currentScoreElement.innerText;
+    //make it a number
+    let currentScore = parseInt(currentScoreText);
+    //set new score
+    let newScore = currentScore + 1;
+    //update new score
+    currentScoreElement.innerText = newScore;
+    continueGame();
+  } else {
+    //get current life
+    let currentLifeElement = document.getElementById("life");
+    let currentLifeText = currentLifeElement.innerText;
+    // make it a number
+    let currentLife = parseInt(currentLifeText);
+    // reduce life
+    let newLife = currentLife - 1;
+    // update new life
+    currentLifeElement.innerText = newLife;
+
+    if (newLife === 0) {
+      gameOver();
+      document
+        .getElementById("play-again")
+        .addEventListener("click", function () {
+          playAgain();
+        });
+    }
+  }
 });
--- js/utility.js
+++ js/utility.js
@@ -1,26 +1,78 @@
 // hide
-function hideClass(elementId){
-    const hide = document.getElementById(elementId);
-    hide.classList.add('hidden');
+function hideClass(elementId) {
+  const hide = document.getElementById(elementId);
+  hide.classList.add("hidden");
 }
 
 // show
-function showClass(elementId){
-    const show = document.getElementById(elementId);
-    show.classList.remove('hidden');
-};
+function showClass(elementId) {
+  const show = document.getElementById(elementId);
+  show.classList.remove("hidden");
+}
 
 //random alphabet generate
-function getRandomAlphabet(){
-    let alphabetsStr = 'abcdefghijklmnopqrstuvwxyz';
-    let alphabetsArr = alphabetsStr.split("");
-    let randomNum = Math.round(Math.random() * 25);
-    let alphabet = alphabetsArr[randomNum];
-    return alphabet;
-};
+function getRandomAlphabet() {
+  let alphabetsStr = "abcdefghijklmnopqrstuvwxyz";
+  let alphabetsArr = alphabetsStr.split("");
+  let randomNum = Math.round(Math.random() * 25);
+  let alphabet = alphabetsArr[randomNum];
+  return alphabet;
+}
 
 // set bg color for selected alphabet
-function setBg(elementId){
-    let element= document.getElementById(elementId);
-    let setBg = element.classList.add('bg-orange-400');
+function setBg(elementId) {
+  let element = document.getElementById(elementId);
+  let setBg = element.classList.add("bg-orange-400");
+  return setBg;
+}
+
+//removeBg
+function removeBg(elementId) {
+  let element = document.getElementById(elementId);
+  let removeBg = element.classList.remove("bg-orange-400");
+  return removeBg;
+}
+
+// play function
+function gameOver() {
+  hideClass("home");
+  hideClass("playground");
+  showClass("final-score");
+  let lastScore = document.getElementById("score").innerText;
+  document.getElementById("last-score").innerText = lastScore;
+}
+
+// play again
+function playAgain() {
+  hideClass("home");
+  hideClass("final-score");
+  showClass("playground");
+  //get current score
+  let currentScoreElement = document.getElementById("score");
+  let currentScoreText = currentScoreElement.innerText;
+  //make it a number
+  let currentScore = parseInt(currentScoreText);
+  currentScore = 0;
+  //set new score
+  let newScore = currentScore;
+  //update new score
+  currentScoreElement.innerText = newScore;
+
+  //get current life
+  let currentLifeElement = document.getElementById("life");
+  let currentLifeText = currentLifeElement.innerText;
+  // make it a number
+  let currentLife = parseInt(currentLifeText);
+  currentLife = 5;
+  // reduce life
+  let newLife = currentLife;
+  // update new life
+  currentLifeElement.innerText = newLife;
+
+  // remove highlight after the game end
+  let currentHighlighted = document
+    .getElementById("alphabetShow")
+    .innerText.toLowerCase();
+  removeBg(currentHighlighted);
+  continueGame();
 }
